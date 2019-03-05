4882cb88b512db56bb2954505a6e9922daffebde
Dexaran <dexaran820@gmail.com>
2019-03-05 04:55:40 +0300

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -5,30 +5,24 @@
 
 using namespace eosio;
 
-class [[eosio::contract("example")]] example_contract : public eosio::contract {
+class [[eosio::contract("resource_delegating")]] example_contract : public eosio::contract {
 
 public:
   using contract::contract;
-  //uint32_t standard_delay = 24 * 60 * 60; // 1 day in seconds
-  uint32_t standard_delay = 180; // 3 minute in seconds for testing purpose only
-  uint64_t request_index = 0;
-  
-  example_contract(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+  uint64_t autodelay = 120;
+  uint64_t counter = 0;
 
-
-  struct [[eosio::table]] request {
-    //uint64_t id;
+ struct [[eosio::table]] request {
     name username;
-    uint64_t CPU;
-    uint64_t NET;
-    uint32_t time;
 
-    uint64_t primary_key() const { return username.value; }
+    uint64_t primary_key() const { return 0; }
 
-    EOSLIB_SERIALIZE( request, (username)(CPU)(NET)(time))
+    EOSLIB_SERIALIZE( request, (username))
   };
 
-  typedef eosio::multi_index<name("request"), request> requests;
+typedef eosio::multi_index<name("request"), request> requests;
+  
+  example_contract(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
   [[eosio::action]]
   void ask(name _user, uint64_t _CPU, uint64_t _NET) {
@@ -36,54 +30,37 @@ public:
     multiauth(_user);
 
     requests reqest_instance(_code, _code.value);
-    auto iterator = reqest_instance.find(_user.value);   
+    auto iterator = reqest_instance.find(0);   
     if( iterator == reqest_instance.end() )
     {
       reqest_instance.emplace(_user, [&]( auto& row ) {
         row.username = _user;
-        row.CPU = _CPU;
-        row.NET = _NET;
-        row.time = now() + standard_delay;
       });
-      delegate_resources( _user, _CPU, _NET, now() + standard_delay);
+      delegate_resources( _user, _CPU, _NET);
       printmsg(_user, "delegated CPU and NET");
     }
     else {
+      printmsg(_user, "contract is currently busy/ resources are not delegated");
       return;
     }
-
-    auto timestamp = now();
   }
 
   [[eosio::action]]
-  void cancel(name _user) {
-
-    multiauth(_user);
-
-    undelegate_resources( _user );
-  }
-
-  [[eosio::action]]
-  void printmsg(name _user, std::string _msg) {
-    require_recipient(_user);
-
-    action(
-      permission_level(name(get_self()), name("active")),
-      get_self(),
-      name("notify"),
-      std::make_tuple(_user, _msg.to_string())
-    ).send();
-  }
-
-  [[eosio::action]]
-  void eraserecord(name _user){
+  void notify(name _user, std::string msg) {
     require_auth(get_self());
+    require_recipient(_user);
+  }
+
+  [[eosio::action]]
+  void onupdate(name _user) {
+    require_auth(get_self());
+    printmsg(_user, std::string("Resources automatically undelegated"));
+    
     requests reqest_instance(_code, _code.value);
-    auto iterator = reqest_instance.find(_user.value);
+    auto iterator = reqest_instance.find(0);
     eosio_assert(iterator != reqest_instance.end(), "Record does not exist");
     reqest_instance.erase(iterator);
-  };
-
+  }
 
 private:
 
@@ -104,56 +81,52 @@ void multiauth(name _user)
     }
 };
 
-void undelegate_resources(name _user){
-  eosio::transaction t{};
-  t.delay_sec = standard_delay;
+void printmsg(name user, std::string _msg) {
+  action(
+    permission_level(name(get_self()), name("active")),
+    get_self(),
+    name("notify"),
+    std::make_tuple(user, _msg)
+  ).send();
+}
 
-  uint64_t amount = 1;
-  asset quantity(amount, symbol("EOS",4));
+void undelegate_resources(name _user, uint64_t _CPU, uint64_t _NET){
+  eosio::transaction t{};
+  t.delay_sec = autodelay; // example - 1 minute delay for the tx
+
+  asset CPU_delegated(_CPU, symbol("EOS",4));
+  asset NET_delegated(_NET, symbol("EOS",4));
 
     t.actions.emplace_back(
       permission_level(name(get_self()), name("active")), 
       name("eosio"),
       name("undelegatebw"), 
-      std::tuple(get_self(), _user, quantity, quantity, false));
+      std::tuple(get_self(), _user, CPU_delegated, NET_delegated, false));
 
-    t.send(now(), name(get_self()));
+    t.actions.emplace_back(
+      permission_level(name(get_self()), name("active")), 
+      name(get_self()),
+      name("onupdate"), 
+      std::tuple(get_self()));
+
+    t.send(now(), name(get_self())); // Send the transaction with some ID derived from the memo
   };
 
-void delegate_resources(name _user, uint64_t _CPU, uint64_t _NET, uint64_t _timeframe){
+void delegate_resources(name _user, uint64_t _CPU, uint64_t _NET){
 
-  asset CPU_delegated(_CPU, symbol("EOS",4));
-  asset NET_delegated(_NET, symbol("EOS",4));
-  action(
-    permission_level(name(get_self()), name("active")), 
-    name("eosio"),
-    name("delegatebw"), 
-    std::tuple(get_self(), _user, NET_delegated, CPU_delegated, false)
-  ).send();
-
-  eosio::transaction tx{};
-  tx.delay_sec = standard_delay;
-
-  tx.actions.emplace_back(
+  asset CPU_to_delegate(_CPU, symbol("EOS",4));
+  asset NET_to_delegate(_NET, symbol("EOS",4));
+    action(
       permission_level(name(get_self()), name("active")), 
       name("eosio"),
-      name("undelegatebw"), 
-      std::tuple(get_self(), _user, NET_delegated, CPU_delegated, false));
+      name("delegatebw"), 
+      std::tuple(get_self(), _user, CPU_to_delegate, NET_to_delegate, false)
+    ).send();
+    counter++;
 
-  tx.actions.emplace_back(
-      permission_level(name(get_self()), name("active")),
-      get_self(),
-      name("notify"),
-      std::make_tuple(_user, "deferred transaction: automated undelegation of bandwidth in 3 minutes"));
+    undelegate_resources(_user, _CPU, _NET);
 
-  tx.actions.emplace_back(
-      permission_level(name(get_self()), name("active")),
-      get_self(),
-      name("eraserecord"),
-      std::make_tuple(_user));
-
-  tx.send(now(), name(get_self()));
   };
 };
 
-EOSIO_DISPATCH( example_contract, (ask)(cancel)(eraserecord) )
+EOSIO_DISPATCH( example_contract, (ask)(notify)(onupdate)(getcounter) )
